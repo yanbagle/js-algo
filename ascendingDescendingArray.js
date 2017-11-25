288306a4d676a99189c48ef06e4e93e5048dbbc2
Zhang <yan.xin.zhang@accenturefederal.com>
2017-11-25 18:59:07 -0500

    ascending descending array

---
+++ ascendingDescendingArray.js
@@ -0,0 +1,38 @@
+function sortAscendingDescendingArray(arr) {
+    if (!arr || arr.length === 0) {
+        return null;
+    }
+    
+    let descendingIndex = -1;
+    let ascendingIndex = -1;
+    
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (ascendingIndex !== -1 && arr[i] < arr[i+1]) {
+            ascendingIndex = i;
+        } else if (descendingIndex !== -1 && arr[i] > arr[i+1]) {
+            descendingIndex = i;               
+        }
+    }
+    
+    let ascendingArr = [];
+    
+    let breakpoint = descendingIndex - ascendingIndex;
+    if (breakpoint < 0) { // the ascending part is in the second half of the array
+        for (let i = ascendingIndex; i < arr.length; i++) {
+            ascendingArr.push(arr[i]);
+        } 
+        for (let i = ascendingIndex - 1; i >= 0; i--) {
+            ascendingArr.push(arr[i]);
+        }
+    } else {
+        for (let i = ascendingIndex; i < breakpoint; i++) {
+            ascendingArr.push(arr[i]);
+        } 
+        for (let i = arr.length - 1; i >= breakpoint; i--) {
+            ascendingArr.push(arr[i]);
+        }
+    }
+    
+    return ascendingArr;
+    
+}
