564172d44182d90bb6f6ece546175b76e904a416
Zhang <yan.xin.zhang@accenturefederal.com>
2017-11-26 20:45:59 -0500

    add linked list

---
+++ addLinkedList.js
@@ -0,0 +1,54 @@
+function LLNode (data) {
+    this.data = data;
+    this.next;
+} 
+
+function addLinkedList(n1, n2) {
+    
+    if (!n1 || !n2) {
+        return null;
+    }
+    
+    let resultNode = new LLNode(null);
+    let head = resultNode;
+    let carry = 0;
+    
+    while (n1 || n2) {
+        let total = carry;
+        if (n1) {
+            total += n1.data;  
+            n1 = n1.next;
+        }
+        if (n2) {
+            total += n2.data;
+            n2 = n2.next;
+        }
+        if (total > 9) {
+            carry = 1;
+            total = total % 10;
+        } else {
+            carry = 0;
+        }
+        
+        resultNode.next = new LLNode(total);
+        resultNode = resultNode.next;
+    }
+    
+    return head.next;
+} 
+
+let ll1 = new LLNode(1);
+ll1.next = new LLNode(1);
+let ll2 = new LLNode(1);
+ll2.next = new LLNode(1);
+
+let total = addLinkedList(ll1, ll2);
+let str = '';
+while (total) {
+    str += total.data;
+    total = total.next;
+}
+
+window.onload = function() {
+    document.getElementById('console').innerHTML = str;
+};
--- ascendingDescendingArray.js
+++ ascendingDescendingArray.js
@@ -36,3 +36,4 @@ function sortAscendingDescendingArray(arr) {
     return ascendingArr;
     
 }
+
