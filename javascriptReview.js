7057a17597526e5fe3933d6f436e054353aea090
Zhang <yan.xin.zhang@accenturefederal.com>
2017-12-29 14:09:49 -0500

    spread and rest

---
+++ javascriptReview.js
@@ -0,0 +1,21 @@
+console.log('number: ' + 2); // number: 2
+console.log('number: ' + 2 + 5); // number: 25
+console.log('1' + 2); // 12
+console.log(3 + 2); // 5
+console.log( '' + 3 + 2); // 32
+console.log(3 + 2 + ''); // 5
+
+console.log('///////////////////////////////////////////////');
+
+function restOperator (...params) {
+    console.log(params.length);
+}
+restOperator('1','2','3');
+
+console.log('///////////////////////////////////////////////');
+
+let obj1 = {a: 1, b: 2};
+let obj2 = {c: 3, d: 4};
+
+let mergedObj =  {...obj1, ...obj2};
+console.log(mergedObj);
