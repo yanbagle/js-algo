3f153c11abf73abb4233ef82f5a884cfaab0a640
Zhang <yan.xin.zhang@accenturefederal.com>
2018-01-09 16:18:39 -0500

    rotate linked list

---
+++ rotateLinkedList.js
@@ -0,0 +1,27 @@
+function rotateLinkedList (linkedList, count) {
+    if (!linkedList || count <= 0) {
+        return null;
+    }
+
+
+    let current = linkedList;
+
+    while (count > 1 && current) {
+        current = current.next;
+        count--;
+    }
+
+    if (!current) {
+        return null;
+    }
+
+    let kthNode = current;
+
+    while (current.next) {
+        current = current.next;
+    }
+
+    current.next = head;
+    head = kthNode.next;
+    kthNode.next = null;
+}
