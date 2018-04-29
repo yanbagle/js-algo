878fc0c4ada94e1683a5f9425d84b4f2e96b5b59
yanzhang000 <circusburger63@yahoo.com>
2018-04-28 23:14:13 -0400

    generate parentheis

--- reverseVowelsInString.js
+++ reverseVowelsInString.js
@@ -10,46 +10,86 @@
 // The vowels does not include the letter "y".
 // A, E, I, O, and U
 
-function reverseVowels(str) {
+// function reverseVowels(str) {
+//
+//     if (!str) {
+//         return null;
+//     }
+//
+//     var vowels = {
+//       a: 'a',
+//       e: 'e',
+//       i: 'i',
+//       o: 'o',
+//       u: 'u'
+//     };
+//
+//     var l = 0;
+//     var r = str.length - 1;
+//
+//     while (l < r) {
+//         if (vowels[str.charAt(l)] && vowels[str.charAt(r)]) {
+//             // switch vowels
+//             var tempLVowel = str.charAt(l);
+//             var tempRVowel = str.charAt(r);
+//             str = replaceChar(str, l, tempRVowel);
+//             str = replaceChar(str, r, tempLVowel);
+//             l++;
+//             r--;
+//         } else {
+//             if (! vowels[str.charAt(l)]) {
+//                 l++;
+//             }
+//             if (! vowels[str.charAt(r)]) {
+//                 r--;
+//             }
+//         }
+//     }
+//
+//     return str;
+// }
+//
+// function replaceChar(str, index, replacement) {
+//     return str.substring(0, index) + replacement + str.substring(index + 1);
+// }
 
+function reverseVowels (str) {
     if (!str) {
-        return null;
+        return;
     }
 
-    var vowels = {
-      a: 'a',
-      e: 'e',
-      i: 'i',
-      o: 'o',
-      u: 'u'
+    let reversedStr = '';
+    const vowels = {
+        a: 'a',
+       e: 'e',
+       i: 'i',
+       o: 'o',
+       u: 'u'
     };
 
-    var l = 0;
-    var r = str.length - 1;
+    let l = 0;
+    let r = str.length - 1;
 
     while (l < r) {
         if (vowels[str.charAt(l)] && vowels[str.charAt(r)]) {
-            // switch vowels
-            var tempLVowel = str.charAt(l);
-            var tempRVowel = str.charAt(r);
-            str = replaceChar(str, l, tempRVowel);
-            str = replaceChar(str, r, tempLVowel);
-            l++;
-            r--;
+            const tempRightChar = str.charAt(r);
+            const tempLeftChar = str.charAt(l);
+            reversedStr = replaceChar(str, l, tempRightChar);
+            reversedStr = replaceChar(str, r, tempLeftChar);
         } else {
-            if (! vowels[str.charAt(l)]) {
+            if (!vowels[str.charAt(l)]) {
                 l++;
             }
-            if (! vowels[str.charAt(r)]) {
+            if (!vowels[str.charAt(r)]) {
                 r--;
             }
         }
     }
 
-    return str;
+    return reversedStr;
 }
 
-function replaceChar(str, index, replacement) {
+function replaceChar (str, index, replacement) {
     return str.substring(0, index) + replacement + str.substring(index + 1);
 }
 
