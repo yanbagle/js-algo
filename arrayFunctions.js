966219139c4d406481bea5db91ddb39a11749cb2
yanzhang000 <circusburger63@yahoo.com>
2018-05-05 21:03:48 -0400

    reduce function

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -26,12 +26,33 @@ MyArray.prototype._forEach = function (func) {
 	}
 };
 
+MyArray.prototype._reduce = function (func, startingValue = this.myArray[0]) {
+    let accumulator = startingValue;
+    let currentIndex = 0;
+    let array = this.myArray;
+    for (let ele of this.myArray) {
+        accumulator = func(accumulator, ele, currentIndex, array);
+        currentIndex++;
+    }
+    return accumulator;
+};
+
 const newArr = new MyArray([1,2,3,4,5]);
 const found = newArr._find((ele) => {
 	return ele === 5;
 });
-console.log(found);
+// console.log(found);
 
 newArr._forEach((ele, index) => {
-	console.log(index + ' ' + ele);
+	// console.log(index + ' ' + ele);
 });
+
+let avg = newArr._reduce((accum, curr, index, array) => {
+    accum += curr;
+    if (index === array.length - 1) {
+        return accum / array.length;
+    }
+    return accum;
+}, 0);
+
+console.log(avg);
