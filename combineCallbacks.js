972751ca4742dd7ca1682da82b4d835bd9cb6124
Zhang <yan.xin.zhang@accenturefederal.com>
2018-05-07 22:36:18 -0400

    combine callbacks

--- combineCallbacks.js
+++ combineCallbacks.js
@@ -1,11 +1,20 @@
 function combineFetchers (fetchers) {
 	return function (pattern, cb) {
 		let results = [];
+		let count = 0;
 		fetchers.forEach((fetcher) => {
 			fetcher(pattern, function (result) {
 				results.concat(result);
+				count++;
+				if (fetchers.length === count) {
+					cb(results);
+				}
 			});
 		});
-		// when done, cb(results);
 	}
 }
+
+const combined = combineFetchers([]);
+combined('pattern', function (results) {
+	// do something with results
+});
