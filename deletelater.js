c8a5368a57384ca2dd22943a5f22f62b568269b8
yan zhang <circusburger63@yahoo.com>
2021-02-09 14:41:31 -0800

    random stuff

--- deletelater.js
+++ deletelater.js
@@ -1,3 +1,30 @@
 const isEven = (num) => {
   return num % 2 === 0;
 };
+
+const arr = [12, 1, 0];
+
+const getLargestNumber = (arr) => {
+  return(arr.sort((a,b) => {
+    return a - b;
+  })[arr.length - 1]);
+};
+
+getLargestNumber(arr);
+
+
+const isOdd = (num) => {
+  return num % 2 !== 0;
+};
+
+const getOddElements = (arr) => {
+  const oddElements = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (isOdd(i+1)) {
+      oddElements.push(arr[i]);
+    }
+  }
+  return oddElements;
+};
+
+console.log(getOddElements([1,2,3,4,5,6,]));
