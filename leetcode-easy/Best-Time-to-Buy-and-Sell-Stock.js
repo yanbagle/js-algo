12d99e252e5f997e6c7e52be57027ffa8005a7d7
yan zhang <circusburger63@yahoo.com>
2021-04-14 13:18:40 -0700

    best time to sell stocks 2

--- leetcode-easy/Best-Time-to-Buy-and-Sell-Stock.js
+++ leetcode-easy/Best-Time-to-Buy-and-Sell-Stock.js
@@ -36,6 +36,40 @@ const maxProfit = (prices) => {
   return maxProfit;
 }
 
+/*
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+*/
+
+const maxProfit2 = (prices) => {
+  if (!prices || prices.length < 2) {
+    return 0;
+  }
+  
+  let profit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    profit += Math.max(0, prices[i] - prices[i-1]);
+  }
+  
+  return profit;
+}
+
+
+
+
+
+
+
 
 
 
