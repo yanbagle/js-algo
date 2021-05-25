5e242e72001da6f2ff95cfb36cdefbe20e3a2186
yan zhang <circusburger63@yahoo.com>
2021-05-25 16:19:27 -0700

    task limiter template

---
+++ leetcode-easy/task-limit-2.js
@@ -0,0 +1,13 @@
+function taskRunner () {
+  const limit = 5;
+  return function runner (task) {
+    
+  }
+}
+
+function myTask () {
+  
+}
+
+let myTaskRunner = taskRunner();
+myTaskRunner(myTask);
