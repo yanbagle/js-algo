9ba7fe4b63d16da8db59c275b79e79ffbf2a85da
yan zhang <circusburger63@yahoo.com>
2021-05-25 17:01:24 -0700

    task limit

--- leetcode-easy/task-limit-2.js
+++ leetcode-easy/task-limit-2.js
@@ -1,13 +1,39 @@
 function taskRunner () {
   const limit = 5;
+  const taskQueue = [];
+  let taskCounter = 0;
+  let counter = 0;
   return function runner (task) {
-    
+    taskCounter++;
+    counter++;
+    if (taskCounter <= limit) {
+      task()(counter, () => { // cb
+        taskCounter--;
+        if (taskCounter < limit && taskQueue.length) {
+          runner(taskQueue.shift());
+        }
+      });
+    } else {
+      taskQueue.push(task);
+    }
   }
 }
 
 function myTask () {
-  
+  return function runTask (counter, cb) {
+		console.log('running ' + counter);
+		setTimeout(() => {
+      console.log(counter + ' done');
+      cb();
+		}, 5000); // does something after 3 secs
+	}
 }
 
 let myTaskRunner = taskRunner();
+myTaskRunner(myTask);
+myTaskRunner(myTask);
+myTaskRunner(myTask);
+myTaskRunner(myTask);
+myTaskRunner(myTask);
+myTaskRunner(myTask);
 myTaskRunner(myTask);
