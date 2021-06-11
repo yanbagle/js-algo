0b09a2fb80981e80e243e806303c9eb52234f426
yan zhang <circusburger63@yahoo.com>
2021-06-10 17:20:54 -0700

    rotate matrix 2

--- leetcode-medium/rotate-image.js
+++ leetcode-medium/rotate-image.js
@@ -9,15 +9,48 @@ const rotate = (matrix) => {
     return [];
   }
   
+  /* 
+  1, 2, 3,
+  4, 5, 6,
+  7, 8, 9 
+  */
+  
   const n = matrix.length;
   
-  for (let i = 0; i < n / 2; i++) {
-		for (let j = 0; j < Math.ceil(((double) n) / 2.); j++) {
-			let temp = matrix[i][j];
-			matrix[i][j] = matrix[n-1-j][i];
-			matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
-			matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
-			matrix[j][n-1-i] = temp;
-		}
-	}
+  /*
+  first transpose the matrix; converting rows into columns
+  1, 4, 7,
+  2, 5, 8,
+  3, 6, 9 
+  */
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) { 
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  
+  /*
+  flip matrix horizontally
+  7, 4, 1,
+  8, 5, 2,
+  9, 6, 3,
+  */
+  
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n / 2; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[i][n - 1 - j];
+      matrix[i][n - 1 - j] = temp;
+    }
+  }
+  
+  return matrix;
 }
+
+
+
+
+
+
