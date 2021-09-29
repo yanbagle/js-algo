29588a712ec5cdb5cacfc90e6db3428f35209645
yan zhang <circusburger63@yahoo.com>
2021-09-29 16:12:17 -0700

    finishing tic tac toe

--- example-app/src/index.js
+++ example-app/src/index.js
@@ -16,9 +16,43 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       xIsNext: true,
+      winner: null,
     };
   }
 
+  calculateWinner (squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    
+    return null;  
+  }
+  
+  checkWinner() {
+    console.log('checking winner');
+    const squares = this.state.squares.slice();
+    const winner = this.calculateWinner(squares);
+    if (!this.state.winner && winner) {
+      this.setState({
+        winner,
+      });
+    }
+  }
+  
   handleClick(i) {
     const squares = this.state.squares.slice();
     if (!squares[i]) {
@@ -26,6 +60,7 @@ class Board extends React.Component {
       this.setState({
         squares: squares,
         xIsNext: !this.state.xIsNext,
+        winnner: null,
       });
     }
   }
@@ -40,7 +75,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    let status = this.state.xIsNext ? 'Next player: X' : 'Next player: O';
+    if (this.state.winner) {
+      console.log('someone won');
+      status = `winner: ${this.state.winner}`
+    } else {
+      this.checkWinner();
+    }
 
     return (
       <div>
