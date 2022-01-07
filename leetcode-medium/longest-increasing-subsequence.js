d934d8d3bc315239fb05d86c16af0026e80c4365
yan zhang <circusburger63@yahoo.com>
2022-01-06 16:36:23 -0800

    longest increaing subseq

---
+++ leetcode-medium/longest-increasing-subsequence.js
@@ -0,0 +1,25 @@
+/*
+
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+*/
+
+const lengthOfLIS = (nums) => {
+  const LIS = Array(nums.length).fill(1);
+    
+  for (let i = nums.length - 1; i >= 0; i--) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] < nums[j]) {
+        LIS[i] = Math.max(LIS[i], 1 + LIS[j]);
+      }
+    }
+  }
+  
+  return Math.max(...LIS);
+}
