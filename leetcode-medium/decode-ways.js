5ab34722864f31d612df615a8574ec09f23a2be7
yan zhang <circusburger63@yahoo.com>
2022-01-13 17:22:28 -0800

    comments

--- leetcode-medium/decode-ways.js
+++ leetcode-medium/decode-ways.js
@@ -6,22 +6,25 @@ https://leetcode.com/problems/decode-ways/
 
 const numDecodings = (s) => {
   const dp = {
-    [s.length] : 1
+    [s.length] : 1 // base ways
   };
   
   const dfs = (i) => {
-    if (dp[i]) {
+    if (dp[i]) { // if already exists, return the previous result
       return dp[i];
     }
-    if (s.charAt(i) === '0') {
+    if (s.charAt(i) === '0') { // 0 is not a valid encoding character 
       return 0;
     }
     
-    let res = dfs(i + 1);
+    let res = dfs(i + 1); // go until we've reached the end 
+    
+    // check to see if we can do 2 number decoding
     if (i + 1 < s.length && 
       (s.charAt(i) === '1' || s.charAt(i) === '2' && '0123456'.includes(s.charAt(i + 1)))) {
       res += dfs(i + 2);
     }
+    
     dp[i] = res;
     return res;
   }
