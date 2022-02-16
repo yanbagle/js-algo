d3736a2d4a3f8e482319cc412435ccb726066ce7
yan zhang <circusburger63@yahoo.com>
2022-02-16 14:29:47 -0800

    reorder list

--- leetcode-medium/rat-in-a-maze.js
+++ leetcode-medium/rat-in-a-maze.js
@@ -4,31 +4,41 @@ https://www.geeksforgeeks.org/rat-in-a-maze-backtracking-2/
 
 */
 
+const x = [
+	[1,1],
+  [0, 1]
+];
+
 const ratInAMaze = (maze) => {
   
   const dfs = (r, c, path) => {
+    if (r === maze.length - 1 && c === maze[0].length - 1 && maze[r][c] === 1) { // found our desination 
+      path[r][c] = 1;
+          console.log(path);
 
+      return true;
+    }
     if (r < 0 || c < 0 || r >= maze.length || c >= maze[0].length || maze[r][c] === 0 || path[r][c] === 1) {
-      return;
+      return false;
     }
     
     path[r][c] = 1;
     
-    dfs(r + 1, c, path);
-    dfs(r - 1, c, path);
-    dfs(r, c + 1, path);
+    const res = dfs(r + 1, c, path) ||
+    dfs(r - 1, c, path) ||
+    dfs(r, c + 1, path) ||
     dfs(r, c - 1, path);
     
     path[r][c] = 0;
+    
+    return res;
   }
   
-  const sol = [];
-  for (let i = 0; i < maze.length; i++) {
-    for (let j = 0; j < maze[0].length; j++) {
-      sol[i][j] = 0;
-    }
-  }
+  const sol = Array(maze.length).fill().map(() => Array(maze[0].length).fill(0));
   
-  dfs(0, 0, sol);
-  return sol;
+	if (dfs(0, 0, sol)) {
+  	return sol;
+  }
 }
+
+ratInAMaze(x);
