f9bc11509aba6ec14e734aa9f273605a44d1a1b3
yan zhang <circusburger63@yahoo.com>
2022-04-20 23:19:17 -0700

    comments

--- leetcode-hard/wild-card.js
+++ leetcode-hard/wild-card.js
@@ -1,5 +1,17 @@
 // https://leetcode.com/problems/wildcard-matching/
 
+/*
+
+#   Using the DFS to solve this problem 
+#   We have a string s abcd and pattern p a*c
+#   Each time we iterate s and p with pointer i and j when s[i] == p[j]
+#   if p[j] is star * which means p[j] can match a string here including empty string
+#   Therefore, first we assuming p[j] match empty then we keep moving two pointers until we cannot have matched part
+#   Then we came back to make let p[j] match s[i] so we keep moving two pointers from i+1 and j
+#   Repeat above steps until there is no more possible matches 
+#   we return false
+
+*/
 const isMatch = (s, p) => {
   let n = s.length;
   let m = p.length;
