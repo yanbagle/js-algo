04b02f413ec8bdbdb953edf64ad66c47d02838e5
yan zhang <circusburger63@yahoo.com>
2024-07-14 20:15:01 -0700

    same tree

---
+++ blind neetcode 75/same-tree.js
@@ -0,0 +1,9 @@
+const isSameTree = (p, q) => {
+  if (!p && !q) {
+    return true;
+  }
+  if (p && q && p.val === q.val) {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+  return false;
+};
