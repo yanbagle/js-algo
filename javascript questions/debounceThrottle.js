136ba237816d304d670c80c9a3c815f6ea4e1487
yan zhang <circusburger63@yahoo.com>
2024-07-21 21:45:21 -0700

    debounce

--- javascript questions/debounceThrottle.js
+++ javascript questions/debounceThrottle.js
@@ -1,17 +1,14 @@
-const debounce = (func, delay) => {
-  let inDebounce;
-  return function () {
-    const context = this;
-    const args = arguments;
-    console.log(inDebounce);
-    clearTimeout(inDebounce); // clears out previous setTimeout function
-    inDebounce = setTimeout(() => {
-      func.apply(context, args);
-    }, delay);
+export function debounce(func, wait) {
+  let timeoutId = null;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, wait);
   };
-};
+}
 
-export default function throttle(func, wait = 0) {
+export function throttle(func, wait = 0) {
   let shouldThrottle = false;
 
   return function (...args) {
@@ -25,43 +22,3 @@ export default function throttle(func, wait = 0) {
     func.apply(this, args);
   };
 }
-
-const throttle2 = function (func, limit) {
-  let timeout;
-  let count = 0; // count is for debugging only
-  let preFunc;
-  return function execute() {
-    preFunc = func;
-    count++;
-    if (!timeout) {
-      func(count);
-      preFunc = null;
-      timeout = true;
-      setTimeout(() => {
-        timeout = false;
-        if (preFunc) {
-          execute();
-        }
-      }, limit);
-    }
-  };
-};
-
-// window.onload = function() {
-// 	const debounceBtn = document.getElementById('debounce');
-// 	debounceBtn.addEventListener('click', debounce(function() {
-// 		console.info('Hey! It is', new Date().toUTCString());
-// 	}, 3000));
-// };
-
-let min = 5000;
-let max = 6000;
-window.onload = function () {
-  const btn = document.getElementById("throttle");
-  btn.addEventListener(
-    "click",
-    throttle2(function (count) {
-      console.log(count);
-    }, Math.floor(Math.random() * (max - min + 1)) + min)
-  );
-};
